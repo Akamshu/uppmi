332b74b4c40290b9af4a97b326352a1ff5f901c6
Akamshu <39032612+Akamshu@users.noreply.github.com>
2023-04-29 10:06:13 +0100

    Add files via upload

--- app/contactForm.php
+++ app/contactForm.php
@@ -1,4 +1,38 @@
+<?php
+if(isset($_POST['submitForm']))
+{
+    $User_name = $_POST['name'];
+    $Phone = $_POST['Phone'];
+    $User_email = $_POST['email'];
+    $User_message = $_POST['message'];
 
+    $email_from = 'noreply@uppmi.com';
+    $email_subject= "UPPMI CONTACT MESSAGE"
+    $email_body ="Name: $User_name.\n".
+                     "Phone No: $Phone.\n".
+                     "Email Id: $User_email.\n".
+                     "User message: $User_message.\n";
+
+    $to_email ="akamshugabriel@gmail.com";
+    $headers = "From: $email_from\r\n";
+    $headers = "Reply-To: $User_email\r\n";
+
+
+    $secretKey = "6LeXhsYlAAAAAK4DJ5zRv79En82zjeUuu4pQKYQO";
+    $responseKey = $_POST['g-recaptcha-response'];
+    $UserIP =$_SERVER['REMOTE_ADDR'];
+    $url = "https://www.google.com/recaptcha/api/siteverify?secret=$secretKey&response=$responseKey&remoteip=$UserIP";
+
+    $response = file_get_contents($url);
+    $response = json_decode($response);
+    if ($response->success){
+        mail($to_email, $email_subject, $email_body, $headers);
+        echo "<span class='alert alert-success'>Message Sent Successfully!</span>";
+    }else{
+        echo "<span class='alert alert-danger'>Message Not Sent</span>";
+    }
+}
+?>
 <!DOCTYPE html>
 <!-- Created By CodingLab - www.codinglabweb.com -->
 <html lang="en" dir="ltr">
@@ -18,62 +52,26 @@
         <div class="address details">
           <i class="fas fa-map-marker-alt"></i>
           <div class="topic">Address</div>
-          <div class="text-one">Igoli, Ogoja</div>
-          <div class="text-two">Cross River State</div>
+          <div class="text-one">Surkhet, NP12</div>
+          <div class="text-two">Birendranagar 06</div>
         </div>
         <div class="phone details">
           <i class="fas fa-phone-alt"></i>
           <div class="topic">Phone</div>
-          <div class="text-one"><a href="tel:+2349046462898" style="text-decoration:none;color:inherit;"> +234 904 646 2898</a></div>
-          <!-- <div class="text-two">+0096 3434 5678</div> -->
+          <div class="text-one">+0098 9893 5647</div>
+          <div class="text-two">+0096 3434 5678</div>
         </div>
         <div class="email details">
           <i class="fas fa-envelope"></i>
           <div class="topic">Email</div>
-          <div class="text-one">uppmi@gmail.com</div>
-          <div class="text-two">info.uppmi@gmail.com</div>
+          <div class="text-one">codinglab@gmail.com</div>
+          <div class="text-two">info.codinglab@gmail.com</div>
         </div>
       </div>
       <div class="right-side">
         <div class="topic-text text-uppercase">SEND US A MESSAGE</div>
-        <div class="status">
-        <!-- <?php
-if(isset($_POST['submitForm']))
-{
-    $User_name = $_POST['name'];
-    $Phone = $_POST['phone'];
-    $User_email = $_POST['email'];
-    $User_message = $_POST['message'];
-
-    $email_from = 'no-reply@uppmi.com';
-    $email_subject= "UPPMI CONTACT MESSAGE";
-    $email_body ="Name: $User_name.\n".
-                     "Phone No: $Phone.\n".
-                     "Email Id: $User_email.\n".
-                     "User message: $User_message.\n";
-
-    $to_email ="akamshugabriel@gmail.com";
-    $headers = "From: $email_from\r\n";
-    $headers .= "Reply-To: $User_email\r\n";
-
-
-    $secretKey = "6LeXhsYlAAAAAK4DJ5zRv79En82zjeUuu4pQKYQO";
-    $responseKey = $_POST['g-recaptcha-response'];
-    $UserIP =$_SERVER['REMOTE_ADDR'];
-    $url = "https://www.google.com/recaptcha/api/siteverify?secret=$secretKey&response=$responseKey&remoteip=$UserIP";
-
-    $response = file_get_contents($url);
-    $response = json_decode($response);
-    if ($response->success){
-        mail($to_email, $email_subject, $email_body, $headers);
-        echo "<h4 class='alert alert-success'>Message Sent Successfully!</h4>";
-    }else{
-        echo "<h4 class='alert alert-danger'>Message Not Sent</h4>";
-    }
-}
-?> -->
-        </div>
-      <form method="post" action="">
+        <!-- <p>Contact us by sending us a message.</p> -->
+      <form method="post" action="contactForm.php">
         <div class="input-box">
           <input type="text" placeholder="Enter your name" name="name" required>
         </div>
@@ -90,7 +88,7 @@ if(isset($_POST['submitForm']))
           <textarea name="message" id="" placeholder="Enter your message here!" cols="30" rows="10" required></textarea>
         </div>
         <!-- RECAPTCHA -->
-        <div class="g-recaptcha" data-sitekey="6LeXhsYlAAAAAAhU18vcLGTwWwX_Rs3sGlFVkZeZ" required></div>
+        <div class="g-recaptcha" data-sitekey="6LeXhsYlAAAAAAhU18vcLGTwWwX_Rs3sGlFVkZeZ"></div>
         <div class="button">
           <input type="submit" value="Send Now" name="submitForm">
         </div>
